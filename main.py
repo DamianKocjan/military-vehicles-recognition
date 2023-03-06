c313454af91d559954a041818b6b60107b63e01d
Damian Kocjan <DamianoKPL@wp.pl>
2023-03-06 23:57:53 +0100

    plot training progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
+import numpy as np
 import tensorflow as tf
+from matplotlib import pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -8,7 +10,7 @@ from tensorflow.keras import layers
 # Load the dataset
 dataset_path = "./datasets"
 vehicle_classes = []  # Initialize an empty list for the classes
-img_size = (400, 400)
+img_size = (256, 256)
 
 # Get the subdirectories in the dataset directory as class names
 for class_name in os.listdir(dataset_path):
@@ -21,6 +23,58 @@ def load_previous_model():
     model.load_weights("vehicle_recognition_model.h5")
 
     return model
+
+
+def unpack_history(history, old_history=None):
+    print(history.history)
+    if old_history is None:
+        new_history = {
+            'accuracy': [],
+            'val_accuracy': [],
+            'loss': [],
+            'val_loss': [],
+        }
+    else:
+        new_history = old_history
+
+    new_history['accuracy'] += history.history['accuracy']
+    new_history['val_accuracy'] += history.history['val_accuracy']
+    new_history['loss'] += history.history['loss']
+    new_history['val_loss'] += history.history['val_loss']
+
+    return new_history
+
+
+def plot_history(training_history):
+    acc = training_history['accuracy']
+    val_acc = training_history['val_accuracy']
+    loss = training_history['loss']
+    val_loss = training_history['val_loss']
+    epochs = np.arange(len(acc)) + 1
+
+    fig = plt.figure(figsize=(12, 4))
+
+    ax1 = fig.add_subplot(121)
+    ax1.plot(epochs, loss, c='g', label='Train')
+    ax1.plot(epochs, val_loss, c='r', label='Valid')
+    ax1.set_title('Loss')
+    ax1.legend(loc='lower left')
+    ax1.grid(True)
+
+    ax2 = fig.add_subplot(122)
+    ax2.plot(epochs, acc, c='g', label='Train')
+    ax2.plot(epochs, val_acc, c='r', label='Valid')
+    ax2.set_title('Accuracy')
+    # ax2.legend(loc='upper left');
+    ax2.grid(True)
+
+    plt.show()
+
+
+def process_and_display(history, old_history=None):
+    new_history = unpack_history(history, old_history)
+    plot_history(new_history)
+    return new_history
 
 
 def create_model():
@@ -83,11 +137,13 @@ model.compile(
 )
 
 # Train the model
-model.fit(
+history = model.fit(
     train_dataset,
     validation_data=validation_dataset,
     epochs=10
 )
+
+tl_history = process_and_display(history)
 
 # Save the model
 model.save("vehicle_recognition_model.h5")
